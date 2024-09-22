ab9f94bd054d1a39fa8e51d203a2ff4ac4fa0550
Eduard Ortegon <eduardortegon2398@gmail.com>
2024-09-22 17:03:57 -0500

    Permitiendo la funcionalidad tanto de Dev, como de produccion y despliegue (arreglado)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/Portfolio_with_UX_UI_Design/' : '/',
+  };
 });
