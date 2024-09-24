13b9ef85dd031f4c7918e3473bce299ac8e7ba80
Eduard Ortegon <eduardortegon2398@gmail.com>
2024-09-24 11:34:36 -0500

    Arreglando boton disponible

--- src/components/landing_content.jsx
+++ src/components/landing_content.jsx
@@ -15,8 +15,8 @@ const LandingContent = () => {
     <div className="lg:pt-[6.5rem] lg:pb-[6.5rem] flex justify-center   self-center  max-h-[calc(100vh - 5.5rem)] justify-center items-center  sm:mt-1">
       <div className="landing-content flex flex-col items-center  w-full text-center ">
         <section className="landing-section w-full max-w-[90%] md:max-w-[65.875rem] min-h-[34.464rem] rounded-[1.5rem] flex flex-col items-center justify-center border-[0.12rem] border-custom-gray-dark bg-[100%_100%,14px_14px] sm:pt-10 sm:pb-10">
-          <div className="tag_container w-[11.5rem] h-[2.06rem] bg-[#1e1e1e] text-white border border-white rounded-[2.25rem] ">
-            <p className="m-0 p-[0.313rem]">Disponible para trabajar</p>
+          <div className="tag_container w-[11.5rem] lg:w-[13.5rem] h-[2.06rem] bg-[#1e1e1e] text-white border border-white rounded-[2.25rem] ">
+            <p className="m-0 p-[0.313rem] w-full text-center">Disponible para trabajar</p>
           </div>
 
           <h1 className="landing-h1 font-bold text-[2.5rem] md:text-[4rem] tracking-widest mb-4">
